38401c42ca5194cbe3bd663d4f79dd44250f46c7
indrikutis <s222958@dtu.dk>
2024-11-18 16:15:31 +0100

    Initial sclera segmentation experiments and ANV sclera color plots without color balancing

---
+++ utils/sclera_segmentation_utils.py
@@ -0,0 +1,74 @@
+import numpy as np
+from skimage import color
+from skimage.io import imread
+
+
+
+def rgb_to_xyy(rgb):
+    """ Convert RGB color to xyY color space.
+
+    Args:
+        rgb (np.array): RGB color values in the range [0, 255].
+
+    Returns:
+        np.array: xyY color values.
+    """
+    rgb = rgb / 255.0 
+    xyz = color.rgb2xyz(rgb)
+    
+    # Convert XYZ to xyY
+    x = xyz[0] / (xyz[0] + xyz[1] + xyz[2])
+    y = xyz[1] / (xyz[0] + xyz[1] + xyz[2])
+    Y = xyz[1]  
+    
+    return np.array([x, y, Y])
+
+def get_average_sclera_color(image_path, l_locations, r_locations):
+
+    """ Get the average sclera color from the image.
+
+    Args:
+        image_path (str): Path to the image.
+        l_locations (list): List of pixel coordinates for the left sclera.
+        r_locations (list): List of pixel coordinates for the right sclera.
+
+    Returns:
+        np.array: Average sclera color in xyY color space.
+    """
+
+    image = imread(image_path)
+
+    # Ensure l_locations and r_locations are NumPy arrays for correct indexing
+    l_locations = np.array(l_locations) if l_locations else None
+    r_locations = np.array(r_locations) if r_locations else None
+
+    l_values = []
+    r_values = []
+
+    # Get the pixel values for the left sclera, if present
+    if l_locations is not None and l_locations.size > 0:
+        h, w = image.shape[:2] 
+        valid_l_locations = (l_locations[:, 0] < w) & (l_locations[:, 1] < h)  
+        l_values = image[l_locations[valid_l_locations, 1], l_locations[valid_l_locations, 0]] 
+
+    # Get the pixel values for the right sclera, if present
+    if r_locations is not None and r_locations.size > 0:
+        valid_r_locations = (r_locations[:, 0] < w) & (r_locations[:, 1] < h)  
+        r_values = image[r_locations[valid_r_locations, 1], r_locations[valid_r_locations, 0]]  
+
+    # Calculate the average sclera color
+    if len(l_values) > 0 and len(r_values) > 0:
+        avg_left = np.mean(l_values, axis=0)
+        avg_right = np.mean(r_values, axis=0)
+        avg_sclera_color = (avg_left + avg_right) / 2
+    elif len(l_values) > 0:  
+        avg_sclera_color = np.mean(l_values, axis=0)
+    elif len(r_values) > 0: 
+        avg_sclera_color = np.mean(r_values, axis=0)
+    else:
+        return None
+
+    # Convert average RGB to xyY
+    avg_xyY = rgb_to_xyy(avg_sclera_color)
+
+    return avg_xyY
